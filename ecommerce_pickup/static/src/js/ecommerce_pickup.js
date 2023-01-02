f43adbcd2ec2a8127813edba57e208e7a65993c6
developer <developer@cantv.net>
2023-01-02 09:51:09 -0400

    [MOD] deploy modal

--- ecommerce_pickup/static/src/js/ecommerce_pickup.js
+++ ecommerce_pickup/static/src/js/ecommerce_pickup.js
@@ -2,10 +2,12 @@ odoo.define('ecommerce_pickup.PickupOptions', function(require) {
     "use strict"
 
     var publicWidget = require('web.public.widget');
-    // var wysiwygLoader = require('web_editor.loader');
 
     publicWidget.registry.PickupOptions = publicWidget.Widget.extend({
         selector: '#pickup',
+        events: {
+            'click #pickup_form_pay': 'pickuPayEvent',
+        },
 
         /**
          * @override
@@ -35,15 +37,22 @@ odoo.define('ecommerce_pickup.PickupOptions', function(require) {
                 var available_qty = option.dataset.qty
                 console.log(available_qty)
                 if (available_qty == undefined || available_qty < total_order_qty){
-                    $('#exampleModalLongTitle').modal('show');
-                    // debugger
+                    var $modal = $('#NonStockModal')
+                    $modal.modal('show');
+                    // var pay = this.counter.appendTo(this.$el);
                 }
             });
+
             return Promise.all([def]);
+        },
+
+        pickuPayEvent: function(e) {
+            var payment = new publicWidget.registry.PaymentForm(this);
+            console.log('payment')
+            console.log(payment)
+            // payment.payEvent()
         }
     });
 
     return publicWidget.registry.PickupOptions;
 });
-
-// https://www.odoo.com/forum/help-1/create-popup-in-website-191760
