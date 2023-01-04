81887820741cc1f6dcf246ccfbad02f941c427f5
developer <developer@cantv.net>
2023-01-04 17:06:34 -0400

    [IMP] Confirm payment action from modal pay button.

--- ecommerce_pickup/static/src/js/ecommerce_pickup.js
+++ ecommerce_pickup/static/src/js/ecommerce_pickup.js
@@ -6,51 +6,55 @@ odoo.define('ecommerce_pickup.PickupOptions', function(require) {
     publicWidget.registry.PickupOptions = publicWidget.Widget.extend({
         selector: '#pickup',
         events: {
-            'click #pickup_form_pay': 'pickuPayEvent',
+            'change #pickup_selection': 'pickSelection',
         },
 
         /**
          * @override
          */
-        start: function() {
+         start: function() {
+            self = this;
             var def = this._super.apply(this, arguments);
             if (this.editableMode) {
                 return def;
             }
-
             console.log('prueba prueba prueba...!')
 
-
-            $('#pickup_selection').change(function (e) {
-                // self = this;
-                // self._rpc({
-                //     model: 'stock.warehouse',
-                //     method: 'search_read',
-                //     fields: ['id','name', 'lot_stock_id'],
-                //     context: self.context,
-                //     // domain: [['id','=',params.id[0]]]
-                // }).then(function(result){
-                //     console.log(result)
-                // })
-                var option = this.options[this.selectedIndex];
-                var total_order_qty = this.dataset.totalQty
-                var available_qty = option.dataset.qty
-                console.log(available_qty)
-                if (available_qty == undefined || available_qty < total_order_qty){
-                    var $modal = $('#NonStockModal')
-                    $modal.modal('show');
-                    // var pay = this.counter.appendTo(this.$el);
-                }
+            $('#pickup_form_pay').click(function (ev) {
+                self.pickuPayEvent(ev);
             });
-
             return Promise.all([def]);
         },
 
-        pickuPayEvent: function(e) {
+        /**
+         * @private
+         * @param {Event} ev
+         */
+
+        pickSelection: function(ev) {
+            var option = ev.currentTarget.options[ev.currentTarget.selectedIndex];
+            var total_order_qty = ev.currentTarget.dataset.totalQty
+            var available_qty = option.dataset.qty
+            console.log(available_qty)
+            if (available_qty == undefined || available_qty < total_order_qty){
+                var $modal = $('#NonStockModal')
+                $modal.modal('show');
+            }
+            return;
+        },
+
+        /**
+         * @private
+         * @param {Event} ev
+         */
+
+        pickuPayEvent: function(ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+            var $form = $('.o_payment_form')
             var payment = new publicWidget.registry.PaymentForm(this);
-            console.log('payment')
-            console.log(payment)
-            // payment.payEvent()
+            payment.el = payment.target = payment.$el = $form
+            return payment.payEvent(ev)
         }
     });
 
